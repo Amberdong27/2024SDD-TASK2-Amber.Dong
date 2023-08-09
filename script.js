8e789ccf200bee0a5872cf7fee824d5d1d502c4b
Amber <136765021+Amberdong27@users.noreply.github.com>
2023-08-10 09:05:08 +1000

    Working on Script.js
    Functioning buttons

---
+++ script.js
@@ -0,0 +1,13 @@
+function gameLoop(playerClicked){
+    if (playerClicked == 'one'){
+        alert ("Hello, World");
+    } else {
+        if (playerClicked == 'two'){
+            alert ("Hello, World 2");
+        } else {
+            if (playerClicked == 'three'){
+                alert ("Hello, World 3");
+            }
+        }
+    }
+}
