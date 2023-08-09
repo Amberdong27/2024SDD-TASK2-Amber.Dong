120583114292ef9d683533e644d8e6ee89ec8a61
Amber <136765021+Amberdong27@users.noreply.github.com>
2023-08-10 09:41:16 +1000

    Working on Script.Js
    Trying to produce a grid

--- script.js
+++ script.js
@@ -1,7 +1,10 @@
+let i = document.getElementById("playerGrid");
+
 function gameLoop(playerClicked){
     if (playerClicked == 'one'){
-        alert ("Hello, World");
-    } else {
+            alert ("grid");
+            i.style.color = "red";
+        } else {
         if (playerClicked == 'two'){
             alert ("Hello, World 2");
         } else {
