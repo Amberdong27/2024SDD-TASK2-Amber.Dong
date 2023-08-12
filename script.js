0cc5322bb292977fb5aacdc363878a168f41ad55
Amber <136765021+Amberdong27@users.noreply.github.com>
2023-08-12 21:04:03 +1000

    Creating grids
    Creating 3x3, 6x6, 9x9 grids

--- script.js
+++ script.js
@@ -1,16 +1,54 @@
-const grid = document.querySelector('.grid');
 
-function gameLoop(playerClicked){
+function makeGrid1 () {
+    for (var i = 0; i < 3; i++) {
+        var row = document.createElement ('div');
+        row.className = "row";
+        for (var j = 0; j < 3; j++){
+            var box = document.createElement ('div');
+            box.className = "box";
+            row.appendChild(box);
+        }
+        document.getElementById('boxParent').appendChild(row);
+
+    } 
+}
+
+function makeGrid2 () {
+    for (var i = 0; i < 6; i++) {
+        var row = document.createElement ('div');
+        row.className = "row";
+        for (var j = 0; j < 6; j++){
+            var box = document.createElement ('div');
+            box.className = "box";
+            row.appendChild(box);
+        }
+        document.getElementById('boxParent').appendChild(row);
+    } 
+}
+
+function makeGrid3 () {
+    for (var i = 0; i < 9; i++) {
+        var row = document.createElement ('div');
+        row.className = "row";
+        for (var j = 0; j < 9; j++){
+            var box = document.createElement ('div');
+            box.className = "box";
+            row.appendChild(box);
+        }
+        document.getElementById('boxParent').appendChild(row);
+    } 
+}
+/*function gameLoop(playerClicked){
     if (playerClicked == 'one'){
             alert ("grid");
-            for (let i = 0; i < 3; i++){
-                for (let j = 0; j < 3; j++){
+           /*for (let i = 0; i < 3; i++){
+                for (let j = 0; j < 3; j++ ){
                     const cell = document.createElement('div');
                     cell.callList.add('cell');
                     grid.appendChild(cell);
-                }
-            }
-        } else {
+                    alert ("creating grid");
+                } 
+            } else {
         if (playerClicked == 'two'){
             alert ("Hello, World 2");
         } else {
@@ -19,4 +57,4 @@ function gameLoop(playerClicked){
             }
         }
     }
-}
+} */
