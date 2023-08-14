84b778fd6b52034efe20ddeae874d3bc73881faf
Amber <136765021+Amberdong27@users.noreply.github.com>
2023-08-14 12:48:52 +1000

    Addition of Array
    added an array for solutions

--- script.js
+++ script.js
@@ -1,23 +1,39 @@
+//9 arrays
 
-function makeGrid1 () {
+var numbers = [
+    "759462813",
+    "463518297",
+    "128973645",
+    "697184532",
+    "584236971",
+    "231795486",
+    "345821769",
+    "912647358",
+    "876359124"
+]
+
+function makeGrid1 () { //board for 3x3 
     document.getElementById("1").disabled = true;
     document.getElementById("2").disabled = true;
     document.getElementById("3").disabled = true;
     document.getElementById("notes").innerHTML = "Rules: Only numbers form 1-9 can be inputted";
     document.getElementById("extra").innerHTML = "Rows and Column must added up to the same number";
     for (var i = 0; i < 3; i++) {
+        // <div id="0" class="row"></div>
         var row = document.createElement ('div');
         row.className = "row";
         for (var j = 0; j < 3; j++){
             var box = document.createElement ('div');
             box.className = "box";
+            box.id = i;
+            box.innerText = numbers [j][i];
             row.appendChild(box);
         }
-        document.getElementById('boxParent').appendChild(row);
+        document.getElementById('board').appendChild(row);
     } 
 }
 
-function makeGrid2 () {
+function makeGrid2 () { //board for 6x6
     document.getElementById("1").disabled = true;
     document.getElementById("2").disabled = true;
     document.getElementById("3").disabled = true;
@@ -30,13 +46,15 @@ function makeGrid2 () {
         for (var j = 0; j < 6; j++){
             var box = document.createElement ('div');
             box.className = "box";
+            box.id = i;
+            box.innerText = numbers [j][i];
             row.appendChild(box);
         }
-        document.getElementById('boxParent').appendChild(row);
+        document.getElementById('board').appendChild(row);
     } 
 }
 
-function makeGrid3 () {
+function makeGrid3 () { //board for 9x9
     document.getElementById("1").disabled = true;
     document.getElementById("2").disabled = true;
     document.getElementById("3").disabled = true;
@@ -48,9 +66,11 @@ function makeGrid3 () {
         for (var j = 0; j < 9; j++){
             var box = document.createElement ('div');
             box.className = "box";
+            box.id = i;
+            box.innerText = numbers [j][i];
             row.appendChild(box);
         }
-        document.getElementById('boxParent').appendChild(row);
+        document.getElementById('board').appendChild(row);
     } 
 }
 
