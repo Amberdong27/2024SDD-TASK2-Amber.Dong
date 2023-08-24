0b5d9c55f052e2562039de348f67244b8cec85cb
Amber <136765021+Amberdong27@users.noreply.github.com>
2023-08-25 09:58:26 +1000

    Removing Button
    Removed the 3 x 3 button

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
 //9 arrays
-
 var solution = [ //9x9 
     "759462813",
     "463518297",
@@ -24,70 +23,23 @@ var puzzle = [
     "8--3--1--"
 ] 
 
-var numSelected = null;
-var tileSelected = null;
+var selectedNumber = 0;
+var tileSelected = 0;
 var x = 0;
 
-function makeGrid1 () { //board for 3x3 
-    document.getElementById("1").disabled = true;
-   // document.getElementById("2").disabled = true;
-    document.getElementById("3").disabled = true;
-    document.getElementById("notes").innerHTML = "Rules: Only numbers form 1-9 can be inputted";
-    document.getElementById("extra").innerHTML = "Rows and Column must added up to the same number";
-       for (var i = 0; i < 3; i++) {
-        // <div id="0" class="row"></div>
-        var row = document.createElement ('div');
-        row.className = "row";
-        // <input id="0" class="box">
-        for (var j = 0; j < 3; j++){
-            var tiles = document.createElement ('div');
-            tiles.maxLength = 1;
-            tiles.className = "box";
-            tiles.id = i.toString() + "-" + j.toString();
-            if (solution[j][i] != "-"){
-                tiles.innerText = solution [j][i];
-            }
-            row.appendChild (tiles);
-            //temporary solution 
-            //box.type = "number";
-           // box.min = "1";
-           // box.max = "9";
-        } 
-        document.getElementById('board').appendChild(row);
-    } 
-        for (var r = 0; r < 1; r++) {
-            for (var c = 1; c < 10; c++) {
-             var numbers = document.createElement ('div');
-                numbers.id = c;
-                numbers.innerText = c;
-        numbers.classList.add("numbers");
-        document.getElementById('choices').append(numbers);
-        }
-    }
-  /*  for (var i = 0; i < 3; i ++) {
-        for (var j = 0; j < 3; j++) {
-            var box = document.createElement ('input');
-            box.id = i.toString() + "-" + j.toString();
-            box.innerText = puzzle [j][i];
-            box.classList.add("box");
-            box.style.height = 200;
-            box.style.width = 200;
-            document.getElementById('board').appendChild(box);
-        } */
-    }
 
 function makeGrid3 () { //board for 9x9
     document.getElementById("1").disabled = true;
-   // document.getElementById("2").disabled = true;
-    document.getElementById("3").disabled = true;
     document.getElementById("notes").innerHTML = "Rules: Only numbers form 1-9 can be inputted";
-    document.getElementById("extra").innerHTML = "Rows and Column must added up to the same number";
-    for (var i = 0; i < 9; i ++) { // Grid 
+    document.getElementById("extra").innerHTML = "Rows and Column must added up to the same number"
+    // Creating grid and generating puzzle
+    for (var i = 0; i < 9; i ++) {  
         for (var j = 0; j < 9; j++) {
             var tiles = document.createElement ('div');
-            tiles.id = i.toString() + "-" + j.toString();
+            tiles.id = i + "-" + j;
             if (puzzle [i][j] != "-") {
                 tiles.innerText = puzzle [i][j];
+                tiles.classList.add("correct-number");
             }
             if (i == 2 || i == 5) {
              tiles.classList.add("horizontal-line");
@@ -100,6 +52,7 @@ function makeGrid3 () { //board for 9x9
         document.getElementById('board').append(tiles);
         }
     }
+    // creating number select
     for (var r = 1; r < 10; r++) { //Choices
         var numbers = document.createElement ('div');
         numbers.id = r;
@@ -111,34 +64,33 @@ function makeGrid3 () { //board for 9x9
     }
     
     function selectNumber(){
-        if (numSelected != null) {
-            numSelected.classList.remove("numbers-selected");
+        if (selectedNumber != 0) {
+            selectedNumber.classList.remove("numbers-selected");
         }
-        numSelected = this;
-        numSelected.classList.add("numbers-selected");
+        selectedNumber = this;
+        selectedNumber.classList.add("numbers-selected");
     }
     
-    function selectTile (){
-        if (numSelected) {
+    function selectTile(){
+        if (selectedNumber) {
             if (this.innerText != ""){
                 return;
             }
+            this.innerText = selectedNumber.id;
             var coords = this.id.split("-");
-            x = this;
             var i = parseInt(coords[0]);
             var j = parseInt(coords[1]);
     
-            if (solution [i][j] == numSelected.id){
-                alert ("correct");
-                x.classList.remove("incorrect-number");
-                this.innerText = numSelected.id;
+            if (solution [i][j] == selectedNumber.id){
+                this.classList.remove("incorrect-number");
+                this.classList.add("correct-number");
+                document.getElementById("feedback").innerHTML = "";
             } else {
-                alert ("incorrect");
-                x.classList.add("incorrect-number");
+                this.classList.add("incorrect-number");
+                document.getElementById("feedback").innerHTML = "Number is incorrect";
+                this.innerText = "";
             }
         }
-    }
-    function removeNumber() {
     }
     function removeGrid() {
         window.location.reload();
@@ -174,25 +126,30 @@ function makeGrid3 () { //board for 9x9
         }
         document.getElementById('board').appendChild(row);
     }
-    } */
-
-/*function makeGrid2 () { //board for 6x6
-    document.getElementById("1").disabled = true;
-    document.getElementById("2").disabled = true;
+    /*function makeGrid1 () { //board for 3x3 
+    document.getElementById("1").disabled = true
     document.getElementById("3").disabled = true;
-    document.getElementById("notes").innerHTML = "Rules: Only numbers form 1-6 can be inputted";
+    document.getElementById("notes").innerHTML = "Rules: Every number can only be inputted once";
     document.getElementById("extra").innerHTML = "Rows and Column must added up to the same number";
-    document.getElementById("extra2").innerHTML = "Each box are 3 x 2";
-    for (var i = 0; i < 6; i++) {
-        var row = document.createElement ('div');
-        row.className = "row";
-        for (var j = 0; j < 6; j++){
-            var box = document.createElement ('div');
-            box.className = "box";
-            box.id = i;
-            box.innerText = numbers [j][i];
-            row.appendChild(box);
+    for (var i = 0; i < 3; i ++ ) {
+        for (var j = 0; j < 3; j++) {
+            var tiles = document.createElement ('div');
+            tiles.id = i + "-" + j;
+            tiles.style.height = "150px";
+            tiles.style.width = "150px";
+            if (puzzle3 [i][j] != "-") {
+                tiles.innerText = puzzle3 [i][j];
+            }
         }
-        document.getElementById('board').appendChild(row);
+        tiles.classList.add("tiles");
+        document.getElementById('board').append(tiles);
     } 
+    for (var r = 1; r < 10; r++) { //Choices
+        var numbers = document.createElement ('div');
+        numbers.id = r;
+        numbers.innerText = r;
+        numbers.classList.add("numbers");
+        document.getElementById('choices').appendChild(numbers);
+        }
 } */
+
